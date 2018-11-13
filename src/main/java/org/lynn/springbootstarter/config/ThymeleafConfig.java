5aee94297871494343de83bbb4d242a9c239eb79
cailinfeng <cailinfeng@mobanker.com>
2018-11-13 22:17:24 +0800

    暂时处理打成jar包无法正常渲染页面的问题

--- src/main/java/org/lynn/springbootstarter/config/ThymeleafConfig.java
+++ src/main/java/org/lynn/springbootstarter/config/ThymeleafConfig.java
@@ -4,9 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
@@ -22,8 +20,8 @@ import java.net.URL;
  * @author : cailinfeng
  * Date : 2018/11/9 5:54 PM
  */
-@Configuration
-@EnableWebMvc
+//@Configuration
+//@EnableWebMvc
 public class ThymeleafConfig extends WebMvcAutoConfiguration {
     @Autowired
     ApplicationContext applicationContext;
@@ -47,7 +45,7 @@ public class ThymeleafConfig extends WebMvcAutoConfiguration {
     private ITemplateResolver templateResolver() {
         SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
         resolver.setApplicationContext(applicationContext);
-        URL resource = this.getClass().getClassLoader().getResource("templates/");       //这里把系统获取到的Class的path替换为源码对应的Path，这样修改的时候就可以动态刷新
+        URL resource = this.getClass().getClassLoader().getResource("/templates/");       //这里把系统获取到的Class的path替换为源码对应的Path，这样修改的时候就可以动态刷新
                 String devResource = resource.getFile().toString().replaceAll("target/classes", "src/main/resources");
         resolver.setPrefix("file:"+devResource);     //不允许缓存
                 resolver.setCacheable(false);
