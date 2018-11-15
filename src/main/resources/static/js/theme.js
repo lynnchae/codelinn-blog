8f66225e8f271a4273a180ef103b9c45f536b125
cailinfeng <cailinfeng@mobanker.com>
2018-11-15 14:18:12 +0800

    添加cookie

--- src/main/resources/static/js/theme.js
+++ src/main/resources/static/js/theme.js
@@ -406,6 +406,8 @@
                 $(".alert-wrapper").fadeOut();
             })
         }
+        $('input[name=commenter]').val(getCookie('commenter'));
+        $('input[name=commenterEmail]').val(getCookie('commenterEmail'));
 
     });
 
@@ -514,3 +516,29 @@ function wechat(){
 
 }
 
+function setCookie(name,value,time) {
+    var strsec = getsec(time);
+    var exp = new Date();
+    exp.setTime(exp.getTime() + strsec*1);
+    document.cookie = name + "="+ escape (value) +";expires=" + exp.toGMTString()+ ";path=/;domain="+document.domain;
+}
+
+function getCookie(name) {
+    var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
+    if(arr=document.cookie.match(reg))
+        return unescape(arr[2]);
+    else
+        return null;
+}
+
+function getsec(str) {
+    var str1=str.substring(1,str.length)*1;
+    var str2=str.substring(0,1);
+    if (str2=="s"){
+        return str1*1000;
+    }else if (str2=="h"){
+        return str1*60*60*1000;
+    }else if (str2=="d"){
+        return str1*24*60*60*1000;
+    }
+}
