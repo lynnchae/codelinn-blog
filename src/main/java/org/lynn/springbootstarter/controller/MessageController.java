178931622832de469d45533eb4283ac40c62543e
cailinfeng <cailinfeng@mobanker.com>
2018-11-28 14:52:32 +0800

    添加serverChan站内消息的微信推送

--- src/main/java/org/lynn/springbootstarter/controller/MessageController.java
+++ src/main/java/org/lynn/springbootstarter/controller/MessageController.java
@@ -1,5 +1,6 @@
 package org.lynn.springbootstarter.controller;
 
+import com.alibaba.druid.util.HttpClientUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.lynn.springbootstarter.common.ResultEntity;
 import org.lynn.springbootstarter.model.Message;
@@ -25,8 +26,16 @@ public class MessageController {
     private MessageService messageService;
 
     @PostMapping("/leavingMessage")
-    public ResultEntity leavingMessage(Message message){
+    public ResultEntity leavingMessage(Message message) {
         messageService.insert(message);
+        StringBuffer param = new StringBuffer("text=");
+        param.append("站内信--来自<").append(message.getSender()).append(">").append("&desp=").append(message.getMessage());
+        try {
+            HttpClientUtils.post("https://sc.ftqq.com/SCU36514Tab24b05f1de7f799500afff8bbe4d2af5bfcf72467b44.send"
+                    , param.toString(), 5000);
+        } catch (Exception e) {
+            log.warn("serverchan sendmessage error, e {}", e);
+        }
         return ResultEntity.success("success");
     }
 
