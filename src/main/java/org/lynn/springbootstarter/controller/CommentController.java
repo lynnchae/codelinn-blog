7c9dfa845913cdcb35f729e0146b1c56f0fbb028
cailinfeng <cailinfeng@mobanker.com>
2018-11-30 12:29:49 +0800

    调整头像随机获取方式

--- src/main/java/org/lynn/springbootstarter/controller/CommentController.java
+++ src/main/java/org/lynn/springbootstarter/controller/CommentController.java
@@ -7,12 +7,17 @@ import org.lynn.springbootstarter.model.Visitor;
 import org.lynn.springbootstarter.service.CommentService;
 import org.lynn.springbootstarter.service.VisitorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -33,15 +38,34 @@ public class CommentController {
     @Autowired
     private VisitorService visitorService;
 
+    private static List<String> headFileNameList;
+
+    private static String IMG_PATH = "/images/head/";
+
+    static {
+        headFileNameList = new ArrayList<>();
+        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();             //将加载多个绝对匹配的所有Resource
+        //将首先通过ClassLoader.getResources("META-INF")加载非模式路径部分      //然后进行遍历模式匹配      Resource[] resources=resolver.getResources("classpath*:META-INF/INDEX.LIST");      Assert.assertTrue(resources.length > 1);          //将加载多个模式匹配的Resource
+        Resource[] resources = new Resource[0];
+        try {
+            resources = resolver.getResources("classpath:static" + IMG_PATH + "*.png");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (Resource r : resources) {
+            headFileNameList.add(r.getFilename());
+        }
+    }
+
     @PostMapping("sendComment")
     public ResultEntity sendComment(Comment comment, HttpServletResponse response) throws IOException {
         Visitor visitor = new Visitor();
         visitor.setName(comment.getCommenter().trim());
-        if(visitorService.count(visitor) > 0){
+        if (visitorService.count(visitor) > 0) {
             visitor = visitorService.query(visitor).get(0);
-        }else{
+        } else {
             Integer fileNameIndex = new Random().nextInt(20) + 1;
-            visitor.setAvatar("/images/head/"+ fileNameIndex +".png");
+            visitor.setAvatar(IMG_PATH + headFileNameList.get(fileNameIndex));
             visitorService.insert(visitor);
         }
         try {
