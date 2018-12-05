381a9cfcda2f48ab1acc265240c573dce85e2e10
cailinfeng <cailinfeng@mobanker.com>
2018-12-05 12:35:25 +0800

    调整首页2张图片随机从8张图片列表中获取，并上传至云服务

--- src/main/java/org/lynn/springbootstarter/controller/IndexController.java
+++ src/main/java/org/lynn/springbootstarter/controller/IndexController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 /**
@@ -27,6 +28,28 @@ import java.util.stream.Collectors;
 @Controller
 @Slf4j
 public class IndexController {
+
+    private static List<String> rollingPicUlrs;
+
+    static {
+        rollingPicUlrs = new ArrayList<>();
+        String url7 = "https://ws2.sinaimg.cn/large/006tNbRwgy1fxvnizat0bj32080go171.jpg";
+        String url6 = "https://ws3.sinaimg.cn/large/006tNbRwgy1fxvnlg1j2xj32080godso.jpg";
+        String url1 = "https://ws2.sinaimg.cn/large/006tNbRwgy1fxvr0yh71dj32080jg7kf.jpg";
+        String url2 = "https://ws2.sinaimg.cn/large/006tNbRwgy1fxvr1iskezj32080gowlr.jpg";
+        String url3 = "https://ws1.sinaimg.cn/large/006tNbRwgy1fxvr2bhn8hj32080goam8.jpg";
+        String url4 = "https://ws2.sinaimg.cn/large/006tNbRwgy1fxvr2j61kxj32080goapd.jpg";
+        String url5 = "https://ws1.sinaimg.cn/large/006tNbRwgy1fxvr2uw6rqj32080goq6x.jpg";
+        String url8 = "https://ws1.sinaimg.cn/large/006tNbRwgy1fxvr3297xrj32080godp5.jpg";
+        rollingPicUlrs.add(url1);
+        rollingPicUlrs.add(url2);
+        rollingPicUlrs.add(url3);
+        rollingPicUlrs.add(url4);
+        rollingPicUlrs.add(url5);
+        rollingPicUlrs.add(url6);
+        rollingPicUlrs.add(url7);
+        rollingPicUlrs.add(url8);
+    }
 
     @Resource
     private BlogService blogService;
@@ -45,6 +68,13 @@ public class IndexController {
         SearchController.copyBlogAndCountComment(list, blogs, c, commentService);
         model.addAttribute("blogs", blogs);
         model.addAttribute("tags", blogService.getTags());
+        Integer fileNameIndex1 = new Random().nextInt(8) + 1;
+        model.addAttribute("pic1", rollingPicUlrs.get(fileNameIndex1));
+        Integer fileNameIndex2 = new Random().nextInt(8) + 1;
+        while (fileNameIndex1 == fileNameIndex2) {
+            fileNameIndex2 = new Random().nextInt(8) + 1;
+        }
+        model.addAttribute("pic2", rollingPicUlrs.get(fileNameIndex2));
         return "index";
     }
 
