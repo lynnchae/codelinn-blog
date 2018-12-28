fb0f2a089aa9dcf7de7b315c772d83ac716f8d2c
cailinfeng <cailinfeng@mobanker.com>
2018-12-28 09:55:13 +0800

    添加百度统计

---
+++ src/main/resources/static/js/baiducount.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?4d24143fc8e3841d9f4bc5157f36e023";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
