bbe10826407402e5361f4144370d87dd4f6409b2
cailinfeng <cailinfeng@mobanker.com>
2019-03-05 18:19:01 +0800

    修复站内信发送的中文乱码问题,添加第三方hutool工具依赖

--- src/main/java/com/codelinn/blog/controller/MessageController.java
+++ src/main/java/com/codelinn/blog/controller/MessageController.java
@@ -1,6 +1,6 @@
 package com.codelinn.blog.controller;
 
-import com.alibaba.druid.util.HttpClientUtils;
+import cn.hutool.http.HttpUtil;
 import com.codelinn.blog.common.ResultEntity;
 import com.codelinn.blog.model.Message;
 import com.codelinn.blog.service.MessageService;
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Class Name : com.codelinn.blog.controller
@@ -28,15 +31,18 @@ public class MessageController {
     @PostMapping("/leavingMessage")
     public ResultEntity leavingMessage(Message message) {
         messageService.insert(message);
-        StringBuffer param = new StringBuffer("text=");
-        param.append("codelinn站内信--来自（").append(message.getSender()).append("）").append("&desp=").append(message.getMessage());
+        Map<String, Object> param = new HashMap<>();
+        param.put("text", "codelinn站内信--来自（" + message.getSender() + "）");
+        param.put("desp", message.getMessage());
         try {
-            HttpClientUtils.post("https://sc.ftqq.com/SCU36514Tab24b05f1de7f799500afff8bbe4d2af5bfcf72467b44.send"
-                    , new String(param.toString().getBytes("UTF-8")), 5000);
+            String url = "https://sc.ftqq.com/SCU36514T84104f04eeffb86aa9baa794c763d8b15c7e3f57cf400.send";
+            String result = HttpUtil.post(url, param);
+            log.info("serverchan message url {}, pushed result {}", url, result);
         } catch (Exception e) {
             log.warn("serverchan sendmessage error, e {}", e);
         }
         return ResultEntity.success("success");
     }
+
 
 }
