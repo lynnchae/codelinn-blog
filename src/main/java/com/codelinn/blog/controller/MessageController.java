6cb740d7dc70d26ac173ac2e6f6f2ac4a3d67854
Linfeng Cai <linn@binance.com>
2020-01-17 17:11:37 +0800

    add check

--- src/main/java/com/codelinn/blog/controller/MessageController.java
+++ src/main/java/com/codelinn/blog/controller/MessageController.java
@@ -1,18 +1,31 @@
 package com.codelinn.blog.controller;
 
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.crypto.SecureUtil;
+import cn.hutool.crypto.digest.HmacAlgorithm;
+import cn.hutool.http.HttpResponse;
 import cn.hutool.http.HttpUtil;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.codelinn.blog.common.ResultEntity;
 import com.codelinn.blog.model.Message;
 import com.codelinn.blog.service.MessageService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 /**
  * Class Name : com.codelinn.blog.controller
@@ -24,12 +37,44 @@ import java.util.Map;
 @RestController()
 @Slf4j
 @RequestMapping("/message")
-public class MessageController {
+public class MessageController implements InitializingBean {
 
     @Autowired
     private MessageService messageService;
 
-    @PostMapping(value = "/leavingMessage",produces = "application/json")
+    private static ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+
+    private static final String API_KEY = "DbXk8h3KSVveLOvR09OhChjIQTuI7eOTp6QM7HeifynyxzJesepKtIxUMPjy0mkW";
+
+    private static final String SECRET = "my04cL1P4IQjFWLq1axnWtu8W0XzgRexNwcYpIbSRcwV1ftulL74l6oa73Vt9vJO";
+
+    private BigDecimal latestFree = BigDecimal.ZERO;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        executorService.scheduleAtFixedRate(() -> {
+            Long currentMillis = System.currentTimeMillis();
+            String sign = SecureUtil.hmac(HmacAlgorithm.HmacSHA256, SECRET).digestHex("recvWindow=5000&timestamp=" + currentMillis);
+            HttpResponse response = HttpUtil.createGet("https://api.binance.com/api/v3/account").body("recvWindow=5000&timestamp=" + currentMillis + "&signature=" + sign).
+                    header("X-MBX-APIKEY", API_KEY).execute();
+            String responseBody = response.body();
+            JSONArray array = JSONObject.parseObject(responseBody).getJSONArray("balances");
+            if (array != null && array.size() > 0) {
+                Object bnbObj = array.stream().filter(item -> JSONObject.parseObject(item.toString()).getString("asset").equalsIgnoreCase("BNB")).collect(Collectors.toList()).get(0);
+                String bnbFree = JSONObject.parseObject(bnbObj.toString()).getString("free");
+                if (new BigDecimal(bnbFree).compareTo(latestFree) != 0) {
+                    String url = "https://sc.ftqq.com/SCU36514T84104f04eeffb86aa9baa794c763d8b15c7e3f57cf400.send";
+                    Map<String, Object> param = new HashMap<>();
+                    param.put("text", DateUtil.format(new Date(), "HH:mm ") + "BNB持仓变动，之前持仓：【" + latestFree + "】，当前持仓：【" + bnbFree + "】");
+                    param.put("desp", "持仓变动");
+                    latestFree = new BigDecimal(bnbFree);
+                    String result = HttpUtil.post(url, param);
+                }
+            }
+        }, 0, 6, TimeUnit.MINUTES);
+    }
+
+    @PostMapping(value = "/leavingMessage", produces = "application/json")
     public ResultEntity leavingMessage(@RequestBody Message message) {
         messageService.insert(message);
         Map<String, Object> param = new HashMap<>();
